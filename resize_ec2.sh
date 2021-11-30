c288abb4aee538edde91d3f6adbe50f6e1502058
Ubuntu <ubuntu@ip-172-31-12-91.eu-central-1.compute.internal>
2021-11-30 15:57:50 +0000

    adding resize_ec2 file

--- resize_ec2.sh
+++ resize_ec2.sh
@@ -1,40 +1,23 @@
-#!/bin/bash
-
-# Specify the desired volume size in GiB as a command line argument. If not specified, default to 50 GiB.
-SIZE=${1:-50}
-
-# Get the ID of the environment host Amazon EC2 instance.
-INSTANCEID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/')
-
-# Get the ID of the Amazon EBS volume associated with the instance.
-VOLUMEID=$(aws ec2 describe-instances \
-  --instance-id $INSTANCEID \
-  --query "Reservations[0].Instances[0].BlockDeviceMappings[0].Ebs.VolumeId" \
-  --output text \
-  --region $REGION)
+#!/usr/bin/env bash
 
 # Resize the EBS volume.
-aws ec2 modify-volume --volume-id $VOLUMEID --size $SIZE
-
+aws ec2 modify-volume --volume-id vol-07a08a5fdb0cc9a49  --size 20
 # Wait for the resize to finish.
 while [ \
   "$(aws ec2 describe-volumes-modifications \
-    --volume-id $VOLUMEID \
+    --volume-id vol-07a08a5fdb0cc9a49 \
     --filters Name=modification-state,Values="optimizing","completed" \
     --query "length(VolumesModifications)"\
     --output text)" != "1" ]; do
 sleep 1
 done
-
 #Check if we're on an NVMe filesystem
-if [[ -e "/dev/xvda" && $(readlink -f /dev/xvda) = "/dev/xvda" ]]
+if [ $(readlink -f /dev/xvda) = "/dev/xvda" ]
 then
   # Rewrite the partition table so that the partition takes up all the space that it can.
   sudo growpart /dev/xvda 1
-
   # Expand the size of the file system.
-  # Check if we're on AL2
+  # Check if we are on AL2
   STR=$(cat /etc/os-release)
   SUB="VERSION_ID=\"2\""
   if [[ "$STR" == *"$SUB"* ]]
@@ -43,11 +26,9 @@ then
   else
     sudo resize2fs /dev/xvda1
   fi
-
 else
   # Rewrite the partition table so that the partition takes up all the space that it can.
   sudo growpart /dev/nvme0n1 1
-
   # Expand the size of the file system.
   # Check if we're on AL2
   STR=$(cat /etc/os-release)
